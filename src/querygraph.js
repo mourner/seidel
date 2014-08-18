f6c66ad762c8e0da550038d55e726f632c6e40e7
Vladimir Agafonkin <agafonkin@gmail.com>
2014-08-18 15:51:32 +0300

    faster and simpler query graph

--- src/querygraph.js
+++ src/querygraph.js
@@ -3,7 +3,7 @@
 module.exports = QueryGraph;
 
 function QueryGraph(head) {
-    this.head = Sink.get(head);
+    this.head = getSink(head);
 }
 
 QueryGraph.prototype = {
@@ -21,42 +21,44 @@ QueryGraph.prototype = {
     },
 
     case1: function (sink, edge, t1, t2, t3, t4) {
-        var yNode = new YNode(edge, Sink.get(t2), Sink.get(t3)),
-            qNode = new XNode(edge.q, yNode, Sink.get(t4)),
-            pNode = new XNode(edge.p, Sink.get(t1), qNode);
+        var yNode = new YNode(edge, getSink(t2), getSink(t3)),
+            qNode = new XNode(edge.q, yNode, getSink(t4)),
+            pNode = new XNode(edge.p, getSink(t1), qNode);
         this.replace(sink, pNode);
     },
 
     case2: function (sink, edge, t1, t2, t3) {
-        var yNode = new YNode(edge, Sink.get(t2), Sink.get(t3)),
-            pNode = new XNode(edge.p, Sink.get(t1), yNode);
+        var yNode = new YNode(edge, getSink(t2), getSink(t3)),
+            pNode = new XNode(edge.p, getSink(t1), yNode);
         this.replace(sink, pNode);
     },
 
     case3: function (sink, edge, t1, t2) {
-        var yNode = new YNode(edge, Sink.get(t1), Sink.get(t2));
+        var yNode = new YNode(edge, getSink(t1), getSink(t2));
         this.replace(sink, yNode);
     },
 
     case4: function (sink, edge, t1, t2, t3) {
-        var yNode = new YNode(edge, Sink.get(t1), Sink.get(t2)),
-            qNode = new XNode(edge.q, yNode, Sink.get(t3));
+        var yNode = new YNode(edge, getSink(t1), getSink(t2)),
+            qNode = new XNode(edge.q, yNode, getSink(t3));
         this.replace(sink, qNode);
     }
 };
 
 
-function Node(lchild, rchild) {
+function YNode(edge, lchild, rchild) {
     this.parents = [];
 
     this.lchild = lchild;
     this.rchild = rchild;
 
-    if (lchild) lchild.parents.push(this);
-    if (rchild) rchild.parents.push(this);
+    lchild.parents.push(this);
+    rchild.parents.push(this);
+
+    this.edge = edge;
 }
 
-Node.prototype.replace = function (node) {
+YNode.prototype.replace = function (node) {
     for (var i = 0; i < node.parents.length; i++) {
         var parent = node.parents[i];
 
@@ -65,7 +67,34 @@ Node.prototype.replace = function (node) {
 
         this.parents.push(parent);
     }
+}
+
+YNode.prototype.locate = function (edge) {
+    if (this.edge.isAbove(edge.p)) return this.rchild.locate(edge);
+    if (this.edge.isBelow(edge.p)) return this.lchild.locate(edge);
+    if (edge.slope < this.edge.slope) return this.rchild.locate(edge);
+    return this.lchild.locate(edge);
 };
+
+
+function XNode(point, lchild, rchild) {
+    this.parents = [];
+
+    this.lchild = lchild;
+    this.rchild = rchild;
+
+    lchild.parents.push(this);
+    rchild.parents.push(this);
+
+    this.x = point.x;
+}
+
+XNode.prototype.locate = function (edge) {
+    if (edge.p.x >= this.x) return this.rchild.locate(edge);
+    return this.lchild.locate(edge);
+};
+
+XNode.prototype.replace = YNode.prototype.replace;
 
 
 function Sink(trapezoid) {
@@ -74,40 +103,12 @@ function Sink(trapezoid) {
     trapezoid.sink = this;
 }
 
-Sink.prototype = Object.create(Node.prototype);
-
 Sink.prototype.locate = function () {
     return this;
 };
 
-Sink.get = function (trapezoid) {
+function getSink(trapezoid) {
     return trapezoid.sink || new Sink(trapezoid);
 };
 
 
-function XNode(point, lchild, rchild) {
-    Node.call(this, lchild, rchild);
-    this.x = point.x;
-}
-
-XNode.prototype = Object.create(Node.prototype);
-
-XNode.prototype.locate = function (edge) {
-    if (edge.p.x >= this.x) return this.rchild.locate(edge);
-    return this.lchild.locate(edge);
-};
-
-
-function YNode(edge, lchild, rchild) {
-    Node.call(this, lchild, rchild);
-    this.edge = edge;
-}
-
-YNode.prototype = Object.create(Node.prototype);
-
-YNode.prototype.locate = function (edge) {
-    if (this.edge.isAbove(edge.p)) return this.rchild.locate(edge);
-    if (this.edge.isBelow(edge.p)) return this.lchild.locate(edge);
-    if (edge.slope < this.edge.slope) return this.rchild.locate(edge);
-    return this.lchild.locate(edge);
-};
